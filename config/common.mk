CyanogenMod/android_vendor_cm

SQUASH: SABERMOD Commits from https://github.com/katinatez/android_vendor_cm credits: pbeeler & kecinzer
Change-Id: Ie70ee1dd67407cd6ee5666e6df8f60c97956ad3a
Signed-off-by: Petr kecinzer Řezníček <kecinzer@gmail.com>

SM script fixes

Change-Id: I7371bb88be96f8f76032e03ccae04feeeffe47c2
Signed-off-by: Petr kecinzer Řezníček <kecinzer@gmail.com>

sm.mk:  use uname -s for UNAME:

uname -sm returns "Linux x86_64" where uname -s returns "Linux"

sm.mk:  Move PRODUCT_PROPERTY_OVERRIDES to proper place

Make sm script compatible with google toolchains

Change-Id: Ibf55ab3684618775e46ed9c55f2af60ca10062f7
Signed-off-by: Petr kecinzer Řezníček <kecinzer@gmail.com>

Final edit of sm.mk

Change-Id: I2eed7d13358b8c1e468892156feaecf84df87f24
Signed-off-by: Petr kecinzer Řezníček <kecinzer@gmail.com>

Change-Id: I4e848c0c061615df8ae75c0a6383906914ef7812

config/common.mk
@@ -250,3 +250,5 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 -include vendor/cyngn/product.mk
 
 $(call prepend-product-if-exists, vendor/extra/product.mk)
+
+-include vendor/cm/config/sm.mk

SlimLG2/vendor_slim

Conflicts:
	config/common.mk

config/common.mk
@@ -209,3 +209,4 @@ FINISHER_SCRIPT := vendor/slim/tools/finisher
 SQUISHER_SCRIPT := vendor/slim/tools/squisher
 CHANGELOG_SCRIPT := vendor/slim/tools/changelog.sh
 
+-include vendor/slim/config/sm.mk